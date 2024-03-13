5550ca41be9032100ffa037afe72c91de4af7829
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    version: Sign every build variant but eng

--- config/version.mk
+++ config/version.mk
@@ -23,7 +23,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.genesis.releasetype=$(GENESIS_BUILDTYPE)
 
 # Signing
-ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/lineage/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
