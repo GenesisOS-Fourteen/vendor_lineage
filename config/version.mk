bakaxgenesis/vendor_genesis

signing: Support inline signing for U
For those who have used .android-certs previously, cp .android-certs/* vendor/genesis/signing/keys/

config/version.mk
@@ -16,11 +16,11 @@ GENESIS_DISPLAY_VERSION := $(GENESIS_VERSION_TAG)-v$(PRODUCT_VERSION_MAJOR).$(PR
 
 # Signing
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+ifneq (,$(wildcard vendor/genesis/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/genesis/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/genesis/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/genesis/signing/keys/otakey.x509.pem
 endif
 endif

GenesisOS-Fourteen/vendor_lineage

config/version.mk
@@ -24,11 +24,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Signing
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+ifneq (,$(wildcard vendor/lineage/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/lineage/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage/signing/keys/otakey.x509.pem
 endif
 endif